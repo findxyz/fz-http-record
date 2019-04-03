e4f3718611779ac2f5e380a3548ff63cabb7c397
fz <natureorz@foxmail.com>
2019-04-03 17:53:03 +0800

    upgrade

--- src/main/java/xyz/fz/controller/RecordController.java
+++ src/main/java/xyz/fz/controller/RecordController.java
@@ -1,7 +1,12 @@
 package xyz.fz.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RecordController {
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
 }
